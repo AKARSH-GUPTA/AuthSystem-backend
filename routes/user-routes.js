d57e5c61697c9622a31655bcbe1ec1cbe1b66b75
AKARSH GUPTA <kg8787054@gmail.com>
2026-04-06 19:56:00 +0530

    Update user-routes.js

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -11,12 +11,22 @@ router.get(
   "/auth/google",
   passport.authenticate("google", { scope: ["profile", "email"] }),
 );
-router.get(
-  "/auth/google/authenticationsystem",
-  passport.authenticate("google", {
-    successRedirect: `${process.env.CLIENT_URL}/`,
-    failureRedirect: `${process.env.CLIENT_URL}/login`,
-  }),
+// router.get(
+//   "/auth/google/authenticationsystem",
+//   passport.authenticate("google", {
+//     successRedirect: `${process.env.CLIENT_URL}/`,
+//     failureRedirect: `${process.env.CLIENT_URL}/login`,
+//   }),
+// );
+router.get("/auth/google/authenticationsystem",
+  passport.authenticate("google", { failureRedirect:`${process.env.CLIENT_URL}/login`}),
+  (req, res) => {
+
+    req.session.save(() => {
+      res.redirect(`${process.env.CLIENT_URL}/`);
+    });
+
+  }
 );
 router.get("/signout",authControllers.Signout);
 router.post("/register",authControllers.Registration);
